b5f0b9a3afb45a0764fee84fd2f38acd22197de6
ThomasKindermann <kindermann@kitconcept.com>
2023-06-25 21:41:23 +0200

    remove unused import

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -1,12 +1,10 @@
 import { Header } from '../features/selectHeader/Header';
 import { Selector } from '../features/selector/Selector';
-import { Link } from 'react-router-dom';
 
 function Select() {
   return (
     <div className="App">
       <Header />
-
       <Selector />
     </div>
   );
